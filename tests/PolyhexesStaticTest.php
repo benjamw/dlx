d9fe63cac5ef8463922e07f7ba5ccedc4f712b87
Benjam Welker <welker@adobe.com>
2025-08-28 09:14:45 -0600

    add more tests

--- tests/PolyhexesStaticTest.php
+++ tests/PolyhexesStaticTest.php
@@ -19,7 +19,18 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(0, $piece);
+
+		$this->assertEquals($piece, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(0, $piece);
@@ -31,14 +42,32 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 0, 1],
 			  [0, 1, 0],
-				[0, 1, 0],
-				  [1, 1, 0],
+			    [0, 1, 0],
+			      [1, 1, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(60, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 1],
+			  [0, 1],
+			    [1, 1],
+			      [0, 1],
 		];
 
 		$returned = Polyhexes::rotatePiece(60, $piece);
@@ -50,13 +79,31 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 0, 0, 1],
 			  [0, 0, 1, 1],
-				[1, 1, 0, 0],
+			    [1, 1, 0, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(-60, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 0, 0, 1],
+			  [0, 0, 1, 0],
+			    [0, 1, 1, 0],
+			      [1, 0, 0, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(-60, $piece);
@@ -68,13 +115,31 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 0, 1, 1],
 			  [1, 1, 0, 0],
-				[1, 0, 0, 0],
+			    [1, 0, 0, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(120, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 0, 0, 1],
+			  [0, 1, 1, 0],
+			    [0, 1, 0, 0],
+			      [1, 0, 0, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(120, $piece);
@@ -86,14 +151,32 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 1, 1],
 			  [0, 1, 0],
-				[0, 1, 0],
-				  [1, 0, 0],
+			    [0, 1, 0],
+			      [1, 0, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(-120, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[1, 0],
+			  [1, 1],
+			    [1, 0],
+			      [1, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(-120, $piece);
@@ -105,13 +188,29 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 1, 0],
 			  [1, 1, 1],
-				[0, 0, 1],
+			    [0, 0, 1],
+		];
+
+		$returned = Polyhexes::rotatePiece(180, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 0, 1, 0],
+			  [1, 1, 1, 1],
 		];
 
 		$returned = Polyhexes::rotatePiece(180, $piece);
@@ -123,13 +222,29 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 1, 0],
 			  [1, 1, 1],
-				[0, 0, 1],
+			    [0, 0, 1],
+		];
+
+		$returned = Polyhexes::rotatePiece(-180, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 0, 1, 0],
+			  [1, 1, 1, 1],
 		];
 
 		$returned = Polyhexes::rotatePiece(-180, $piece);
@@ -141,14 +256,32 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 1, 1],
 			  [0, 1, 0],
-				[0, 1, 0],
-				  [1, 0, 0],
+			    [0, 1, 0],
+			      [1, 0, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(240, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[1, 0],
+			  [1, 1],
+			    [1, 0],
+			      [1, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(240, $piece);
@@ -160,13 +293,31 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 0, 0, 1],
 			  [0, 0, 1, 1],
-				[1, 1, 0, 0],
+			    [1, 1, 0, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(300, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[0, 0, 0, 1],
+			  [0, 0, 1, 0],
+			    [0, 1, 1, 0],
+			      [1, 0, 0, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(300, $piece);
@@ -178,13 +329,29 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(360, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
+		];
+
+		$expected = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(360, $piece);
@@ -207,7 +374,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[0, 1, 1],
 			  [1, 0, 1],
-				[1, 1, 0],
+			    [1, 1, 0],
 		];
 
 		$returned = Polyhexes::rotatePiece(60, $piece);
@@ -219,7 +386,22 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
+		];
+
+		// Rotate 60 degrees 6 times should return to original
+		$rotated = $piece;
+		for ($i = 0; $i < 6; $i++) {
+			$rotated = Polyhexes::rotatePiece(60, $rotated);
+		}
+
+		$this->assertEquals($piece, $rotated);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
 		];
 
 		// Rotate 60 degrees 6 times should return to original
@@ -235,7 +417,22 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
+		];
+
+		// Rotate 120 degrees 3 times should return to original
+		$rotated = $piece;
+		for ($i = 0; $i < 3; $i++) {
+			$rotated = Polyhexes::rotatePiece(120, $rotated);
+		}
+
+		$this->assertEquals($piece, $rotated);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
 		];
 
 		// Rotate 120 degrees 3 times should return to original
@@ -251,7 +448,22 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
+		];
+
+		// Rotate 180 degrees 2 times should return to original
+		$rotated = $piece;
+		for ($i = 0; $i < 2; $i++) {
+			$rotated = Polyhexes::rotatePiece(180, $rotated);
+		}
+
+		$this->assertEquals($piece, $rotated);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1, 0, 0],
 		];
 
 		// Rotate 180 degrees 2 times should return to original
@@ -273,66 +485,36 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$this->assertEquals($piece, $returned);
 	}
 
-	public function testRotatePieceHorizontalLine() {
-		$piece = [
-			[1, 1, 1],
-		];
-
-		$expected60 = [
-			[1],
-			  [1],
-				[1],
-		];
-
-		$returned = Polyhexes::rotatePiece(60, $piece);
-
-		$this->assertEquals($expected60, $returned);
-	}
-
-	public function testRotatePieceHorizontalLine120() {
-		$piece = [
-			[1, 1, 1],
-		];
-
-		$expected120 = [
-			[0, 0, 1],
-			  [0, 1, 0],
-				[1, 0, 0],
-		];
-
-		$returned = Polyhexes::rotatePiece(120, $piece);
-
-		$this->assertEquals($expected120, $returned);
-	}
-
-	public function testRotatePieceVerticalLine() {
-		$piece = [
-			[1],
-			  [1],
-				[1],
-		];
-
-		$expected120 = [
-			[1, 1, 1],
-		];
-
-		$returned = Polyhexes::rotatePiece(120, $piece);
-
-		$this->assertEquals($expected120, $returned);
-	}
-
 	public function testRotatePieceIrregularShape() {
 		$piece = [
 			[1],
 			  [1, 1, 1],
-				[0, 1],
+			    [0, 1],
 		];
 
 		$expected = [
 			[0, 0, 1],
 			  [0, 1, 0],
-				[0, 1, 0],
-				  [1, 1, 0],
+			    [0, 1, 0],
+			      [1, 1, 0],
+		];
+
+		$returned = Polyhexes::rotatePiece(60, $piece);
+
+		$this->assertEquals($expected, $returned);
+
+		// ---------------------------------------------------------
+
+		$piece = [
+			[1, 1, 1, 1],
+			  [0, 1],
+		];
+
+		$expected = [
+			[0, 1],
+			  [0, 1],
+			    [1, 1],
+			      [0, 1],
 		];
 
 		$returned = Polyhexes::rotatePiece(60, $piece);
@@ -605,14 +787,14 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 0],
 			  [1, 1, 1],
-				[0, 1, 0],
+			    [0, 1, 0],
 		];
 
 		$expected = [
 			[0, 0, 1],
 			  [0, 1, 0],
-				[0, 1, 0],
-				  [1, 1, 0],
+			    [0, 1, 0],
+			      [1, 1, 0],
 		];
 
 		// Use reflection to access protected method
@@ -629,7 +811,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 1, 1],
 			  [1, 0, 1],
-				[1, 1, 1],
+			    [1, 1, 1],
 		];
 
 		$expected = [
@@ -655,7 +837,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 1],
 			  [1, 0, 1, 1],
-				[1, 0, 1, 0],
+			    [1, 0, 1, 0],
 		];
 
 		$expected = [
@@ -692,7 +874,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[0, 0, 0],
 			  [0, 0, 0],
-				[0, 0, 0],
+			    [0, 0, 0],
 		];
 
 		$expected = [];
@@ -734,7 +916,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$expected = [
 			[1, 1, 1],
 			  [1, 0, 1],
-				[1, 1, 1],
+			    [1, 1, 1],
 		];
 
 		$returned = Polyhexes::cubicCoordinatesToArray($cubicCoords);
@@ -763,7 +945,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$expected = [
 			[1, 1, 0, 0],
 			  [1, 0, 1, 1],
-				[1, 0, 1, 0],
+			    [1, 0, 1, 0],
 		];
 
 		$returned = Polyhexes::cubicCoordinatesToArray($cubicCoords);
@@ -799,7 +981,7 @@ class PolyhexesStaticTest extends \PHPUnit\Framework\TestCase {
 		$piece = [
 			[1, 0, 1],
 			  [1, 0, 0],
-				[0, 1, 1],
+			    [0, 1, 1],
 		];
 
 		$cubicCoords = Polyhexes::arrayToCubicCoordinates($piece);
